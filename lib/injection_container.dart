a8b09274246e2b808f7c19ea086a00c35477d104
yieie <ison632413672@gmail.com>
2025-05-24 15:10:02 +0800

    feat(mock): 加入 getUserInfo 的 interceptor 假回應

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,27 +2,31 @@
 import 'package:dio/dio.dart';
 import 'package:front_end/features/data/datasources/remote/ann_api_service.dart';
 import 'package:front_end/features/data/datasources/remote/auth_api_service.dart';
+import 'package:front_end/features/data/datasources/remote/user_management_api_service.dart';
 import 'package:front_end/features/data/datasources/remote/workshop_api_service.dart';
 import 'package:front_end/features/data/repositories/ann_repository_impl.dart';
 import 'package:front_end/features/data/repositories/auth_repository_impl.dart';
+import 'package:front_end/features/data/repositories/user_management_repositoryImpl.dart';
 import 'package:front_end/features/data/repositories/workshop_repository_impl.dart';
 import 'package:front_end/features/domain/repositories/ann_repository.dart';
 import 'package:front_end/features/domain/repositories/auth_repository.dart';
+import 'package:front_end/features/domain/repositories/user_management_repository.dart';
 import 'package:front_end/features/domain/repositories/workshop_repository.dart';
 import 'package:front_end/features/domain/usecases/get_10_announcement.dart';
 import 'package:front_end/features/domain/usecases/get_detail_announcement.dart';
 import 'package:front_end/features/domain/usecases/get_workshop.dart';
 import 'package:front_end/features/domain/usecases/get_workshop_participation.dart';
 import 'package:front_end/features/domain/usecases/join_workshop.dart';
+import 'package:front_end/features/domain/usecases/search_user_by_uid.dart';
 import 'package:front_end/features/domain/usecases/sign_in.dart';
 import 'package:front_end/features/domain/usecases/sign_up.dart';
 import 'package:front_end/features/presentation/bloc/ann_bloc.dart';
 import 'package:front_end/features/presentation/bloc/auth/auth_bloc.dart';
 import 'package:front_end/features/presentation/bloc/auth/sign_in_bloc.dart';
 import 'package:front_end/features/presentation/bloc/auth/sign_up_bloc.dart';
+import 'package:front_end/features/presentation/bloc/user_management/search_user_bloc.dart';
 import 'package:front_end/features/presentation/bloc/workshop/workshop_list_bloc.dart';
 import 'package:front_end/features/presentation/bloc/workshop/workshop_participation_bloc.dart';
-import 'package:front_end/features/presentation/bloc/workshop/workshop_participation_event.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -39,12 +43,44 @@ Future<void> initializeDependencies() async {
       error: true,            // 是否打印錯誤信息
     ),
   );
+ /*  
+ //攔截回應測試json解析是否正確
+  dio.interceptors.add(InterceptorsWrapper(
+    onRequest: (options, handler) {
+      print('🚀 [Mock] intercepting request: ${options.uri}');
+      if (options.uri.path.contains('getUserInfo.php')) {
+        print('🧪 Intercepting: ${options.uri}');
+        // 攔截，直接回假資料
+        return handler.resolve(Response(
+          requestOptions: options,
+          statusCode: 200,
+          data: {
+            "success": true,
+            "uId": "a1115526",
+            "name": "陳小明",
+            "email": "a1115526@gmail.com",
+            "phone": "0912345678",
+            "sexual": "男",
+            "userType": "student",
+            "studentInfo": {
+              "department": "資訊工程學系",
+              "grade": "大五"
+            },
+          },
+        ));
+      }
+      return handler.next(options);
+    },
+  )); 
+  */
+
   
   sl.registerSingleton<Dio>(dio);
 
   sl.registerSingleton<AnnApiService>(AnnApiService(sl()));
   sl.registerSingleton<AuthApiService>(AuthApiService(sl()));
   sl.registerSingleton<WorkshopApiService>(WorkshopApiService(sl()));
+  sl.registerSingleton<UserManagementApiService>(UserManagementApiService(sl()));
 
   sl.registerSingleton<AnnRepository>(
     AnnRepositoryImpl(sl())
@@ -54,6 +90,9 @@ Future<void> initializeDependencies() async {
   );
   sl.registerSingleton<WorkshopRepository>(
     WorkshopRepositoryImpl(sl())
+  );
+  sl.registerSingleton<UserManagementRepository>(
+    UserManagementRepositoryimpl(sl())
   );
 
   sl.registerSingleton<Get10AnnouncementUseCase>(
@@ -77,6 +116,9 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<JoinWorkshopUseCase>(
     JoinWorkshopUseCase(sl())
   );
+  sl.registerSingleton<SearchUserByUidUseCase>(
+    SearchUserByUidUseCase(sl())
+  );
 
   sl.registerFactory<AuthBloc>(
     ()=> AuthBloc()
@@ -96,21 +138,7 @@ Future<void> initializeDependencies() async {
   sl.registerFactory<WorkshopParticipationBloc>(
     () => WorkshopParticipationBloc(sl(),sl())
   );
-}
-
-void setupDio() {
-  final dio = Dio();
-
-  dio.interceptors.add(
-    LogInterceptor(
-      requestBody: true,      // 是否打印請求的 body
-      responseBody: true,     // 是否打印回應的 body
-      requestHeader: true,    // 是否打印請求頭
-      responseHeader: true,   // 是否打印回應頭
-      error: true,            // 是否打印錯誤信息
-    ),
+  sl.registerFactory<SearchUserBloc>(
+    () => SearchUserBloc(sl())
   );
-
-  // 註冊 Dio 實例到 DI 容器
-  sl.registerSingleton<Dio>(dio);
 }
