325780e15ebe241154e405ba8072a17a1497ee47
yieie <ison632413672@gmail.com>
2025-06-01 04:43:10 +0800

    feat: 新增SDG類別

---
+++ lib/features/domain/entity/SdgItem.dart
@@ -0,0 +1,13 @@
+class SdgsItem {
+  final int id;
+  final String name;
+  final String imagePath;
+
+  SdgsItem({
+    required this.id,
+    required this.name,
+    required this.imagePath,
+  });
+}
+
+
