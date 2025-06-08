a26d9fad0ddc7d15376733d8afda69af793d7de0
yieie <ison632413672@gmail.com>
2025-06-08 20:42:02 +0800

    feat : 修改資料解析欄位，新增資料解析方式(toJson、fromEntity)

--- lib/features/data/models/announcement.dart
+++ lib/features/data/models/announcement.dart
@@ -24,7 +24,7 @@ class AnnouncementModel extends Announcement{
       aid: json['aId'],
       title: json['title'] ?? "",
       content: json['content'] ?? "",
-      time: json['time'] ?? "",
+      time: json['publishDate'] ?? "",
       posterUrl: json['posterUrls'] != null
         ? List<String>.from(json['posterUrls'])
         : [],
@@ -38,6 +38,27 @@ class AnnouncementModel extends Announcement{
         }).toList()
         :[]
     );
+  }
 
+  Map<String, dynamic> toJson(){
+    return {
+      "aId":aid,
+      "annTitle": title,
+      "annContent": content,
+      "publishDate": time,
+      "uId": uid
+    };
+  }
+  
+  factory AnnouncementModel.fromEntity(Announcement ann){
+    return AnnouncementModel(
+      aid: ann.aid,
+      title: ann.title,
+      content: ann.content,
+      time: ann.time,
+      uid:ann.uid,
+      posterUrl: ann.posterUrl,
+      file: ann.file
+    );
   }
 }
